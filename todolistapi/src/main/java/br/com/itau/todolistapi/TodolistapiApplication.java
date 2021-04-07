072b8aca128d1e56959b0011e3935ab01c8676f7
kadu henrique <ka.furtado.s2@gmail.com>
2021-04-07 02:44:20 -0300

    ciração cliente controller

---
+++ todolistapi/src/main/java/br/com/itau/todolistapi/TodolistapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.itau.todolistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistapiApplication.class, args);
+	}
+
+}
